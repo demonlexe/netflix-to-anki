19cc0ff15e241214774c9110f6f66acc14247fb8
demonlexe <alexiskaufman@gmail.com>
2024-07-15 21:44:26 -0500

    ask gemini for dynamic translation

--- background/messages/spanishdict.ts
+++ background/messages/spanishdict.ts
@@ -8,6 +8,14 @@ export type RequestBody = {
 
 export type RequestResponse = string
 
+async function getLocaleFromModel(model: any, phrase: string) {
+  const promptForLocale =
+    "What language is the following phrase in? Respond with 'es' or 'en'."
+  const localeResult = await model.generateContent([promptForLocale, phrase])
+  const regex = new RegExp("es", "gi")
+  return regex.test(localeResult.response.text()) ? "es" : "en"
+}
+
 const handler: PlasmoMessaging.MessageHandler<
   RequestBody,
   RequestResponse
@@ -17,14 +25,16 @@ const handler: PlasmoMessaging.MessageHandler<
   const { GoogleGenerativeAI } = require("@google/generative-ai")
 
   const genAI = new GoogleGenerativeAI(process.env.PLASMO_PUBLIC_GEMINI_TOKEN)
-  console.log("TOKEN: ", process.env.PLASMO_PUBLIC_GEMINI_TOKEN)
-
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
-  const result = await model.generateContent([
-    "Please translate the following phrase to english or spanish, depending on what language it is currently in. Respond with just the translated text.",
-    phrase
-  ])
+  const locale = await getLocaleFromModel(model, phrase)
+  const prompt =
+    locale === "es"
+      ? "Please translate the following phrase to english. Respond with just the translated text. If there are times or numbers, please write them out. If there are multiple possible translations, please provide only one."
+      : "Please translate the following phrase to spanish. Respond with just the translated text. If there are times or numbers, please write them out. If there are multiple possible translations, please provide only one."
+
+  const result = await model.generateContent([prompt, phrase])
   console.log(result.response.text())
+  res.send(result.response.text())
 }
 
 export default handler
