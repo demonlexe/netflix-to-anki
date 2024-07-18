c5e5bc432aebb0d0209ba8e7186bba3d04ebd67f
demonlexe <alexiskaufman@gmail.com>
2024-07-18 16:45:42 -0500

    add comments

--- contents/index.ts
+++ contents/index.ts
@@ -62,10 +62,15 @@ window.addEventListener("message", async (event) => {
 })
 
 function updateTranslations(currentText: string, translatedText: string) {
+    // pre-processing
     currentText = currentText.trim()
     translatedText = translatedText.trim()
+
+    // update the displayed text
     const liveElement = $(`span:contains("${currentText}")`).find("span").last()
     changeText(liveElement, translatedText)
+
+    // update the cache
     localTranslations[currentText] = translatedText
     reverseTranslations[translatedText] = currentText
 }
