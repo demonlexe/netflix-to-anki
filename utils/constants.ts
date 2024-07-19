aba87aedab20603c3523371df067ac6558b3ea81
demonlexe <alexiskaufman@gmail.com>
2024-07-18 21:54:25 -0500

    lower the batch size

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 import type { UserSettings } from "~utils/localData"
 
-export const BATCH_SIZE = 175
+export const BATCH_SIZE = 145
 export const USER_SETTINGS_DEFAULTS: UserSettings = {
     TARGET_LANGUAGE: "en-US",
     API_KEY: "",
