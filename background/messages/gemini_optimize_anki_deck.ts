d35e8d76527905d2363b86b743e1533a8e38860d
demonlexe <alexiskaufman@gmail.com>
2024-07-20 14:55:59 -0500

    improve gemini prompt

--- background/messages/gemini_optimize_anki_deck.ts
+++ background/messages/gemini_optimize_anki_deck.ts
@@ -8,10 +8,12 @@ import { getData } from "~utils/localData"
 
 const geminiPrompt = (deck: Record<string, string>) =>
     [
-        "Please optimize the following anki deck, and return the optimized deck. It should be in the same JSON format.",
-        "Please remove any duplicate phrases, including those that are translated reversely to the same phrase.",
-        "Do not include ANY formatting markdown such as '```json', but please make sure the response is valid json with proper escape characters when needed.",
-        `Try very hard to avoid errors like "Expected ':' after property name in JSON at position 7049" and "Expected ',' or '}' after property value in JSON at position 6594 (line 1 column 6595)"`,
+        "Please optimize the following language learning Anki deck and return the optimized deck in the same JSON format. To optimize the deck, please use your own discretion, but also:",
+        "\n",
+        "1. Remove any duplicate phrases.",
+        "2. Do not include ANY formatting markdown such as '```json'",
+        "3. Maintain the standard of mapping the target language to the native language.",
+        "\n",
         JSON.stringify(deck)
     ].join("\n")
 
@@ -20,13 +22,19 @@ const handler: PlasmoMessaging.MessageHandler<
     GeminiOptimizeAnkiDeckResponse
 > = async (req, res) => {
     try {
+        console.log("Deck before optimization: ", req.body.deck)
         const genAI = new GoogleGenerativeAI(await getData("API_KEY"))
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
         const newDeck = await model.generateContent([
             geminiPrompt(req.body.deck)
         ])
+        console.log(
+            "Gemini Optimization Result: ",
+            JSON.parse(newDeck.response?.text())
+        )
         res.send({ deck: JSON.parse(newDeck.response?.text()) })
     } catch (e) {
+        console.log("ERROR: ", e)
         res.send({ error: e?.message })
         return
     }
