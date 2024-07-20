8dfeb9a10acd0de244dec0aa940d0ae032b466ae
demonlexe <alexiskaufman@gmail.com>
2024-07-20 13:52:08 -0500

    add trimming

--- utils/functions/createAnkiDeck.ts
+++ utils/functions/createAnkiDeck.ts
@@ -4,8 +4,8 @@ export default async function createAnkiDeck(): Promise<File> {
     const [NEED_TO_STUDY] = await Promise.all([getData("NEED_TO_STUDY")])
     const allCards = []
     for (const [key, value] of Object.entries(NEED_TO_STUDY ?? {})) {
-        const newKey = key?.replace(/\n/g, " ")?.replace(/\t/g, " ")
-        const newValue = value?.replace(/\n/g, " ")?.replace(/\t/g, " ")
+        const newKey = key?.replace(/\n/g, " ")?.replace(/\t/g, " ")?.trim()
+        const newValue = value?.replace(/\n/g, " ")?.replace(/\t/g, " ")?.trim()
         if (!newKey || !newValue) continue
 
         allCards.push(`${newKey}\t${newValue}`)
