91dcf635611ff240cab4ffe772224a1a3dc0a24a
demonlexe <alexiskaufman@gmail.com>
2024-07-20 13:40:12 -0500

    nit fixes

--- background/messages/gemini_translate.ts
+++ background/messages/gemini_translate.ts
@@ -11,7 +11,7 @@ import { getData } from "~utils/localData"
 
 const TranslationRequirements = (language: string) =>
     [
-        `The main objective is to translate the following phrase to ${language}.`,
+        `The main objective is to translate the following phrases to ${language}.`,
         "If there are times or numbers, please write them out.",
         "Do not include ANY formatting markdown such as '```json', but please make sure the response is valid json with proper escape characters when needed.",
         `Try very hard to avoid errors like "Expected ':' after property name in JSON at position 7049" and "Expected ',' or '}' after property value in JSON at position 6594 (line 1 column 6595)"`,
