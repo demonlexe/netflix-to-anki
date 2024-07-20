49c99b23e5d7bd5acf90d5f33f270663518ab883
demonlexe <alexiskaufman@gmail.com>
2024-07-20 15:40:27 -0500

    create different button to click if wanting to get optimized deck.

--- utils/functions/createAnkiDeck.ts
+++ utils/functions/createAnkiDeck.ts
@@ -3,21 +3,27 @@ import { sendToBackground } from "@plasmohq/messaging"
 import type { GeminiOptimizeAnkiDeckResponse } from "~background/types/GeminiOptimizeAnkiDeckResponse"
 import { getData } from "~utils/localData"
 
-export default async function createAnkiDeck(): Promise<File> {
+export default async function createAnkiDeck(
+    useOptimized: boolean
+): Promise<File> {
     const [NEED_TO_STUDY] = await Promise.all([getData("NEED_TO_STUDY")])
-    // optimize with Gemini
-    const optimizedDeckResponse: GeminiOptimizeAnkiDeckResponse =
-        await sendToBackground({
-            name: "gemini_optimize_anki_deck",
-            body: { deck: NEED_TO_STUDY }
-        })
-    if (!optimizedDeckResponse || optimizedDeckResponse.error) {
-        return null
+    let deckToUse = NEED_TO_STUDY
+    if (useOptimized) {
+        // optimize with Gemini
+        const optimizedDeckResponse: GeminiOptimizeAnkiDeckResponse =
+            await sendToBackground({
+                name: "gemini_optimize_anki_deck",
+                body: { deck: NEED_TO_STUDY }
+            })
+        if (!optimizedDeckResponse || optimizedDeckResponse.error) {
+            return null
+        }
+        deckToUse = optimizedDeckResponse.deck
     }
-    const optimizedDeck = optimizedDeckResponse.deck
+
     // Then build the file
     const allCards = []
-    for (const [key, value] of Object.entries(optimizedDeck ?? {})) {
+    for (const [key, value] of Object.entries(deckToUse ?? {})) {
         const newKey = key?.replace(/\n/g, " ")?.replace(/\t/g, " ")?.trim()
         const newValue = value?.replace(/\n/g, " ")?.replace(/\t/g, " ")?.trim()
         if (!newKey || !newValue) continue
