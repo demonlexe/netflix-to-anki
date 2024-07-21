613e9b097a790ab5c86c3ed56d1dd55658e106a2
demonlexe <alexiskaufman@gmail.com>
2024-07-21 08:15:09 -0500

    keep break tags

--- background/messages/catch_netflix_subtitles.ts
+++ background/messages/catch_netflix_subtitles.ts
@@ -4,19 +4,33 @@ import type { PlasmoMessaging } from "@plasmohq/messaging"
 
 import { type CatchNetflixSubtitlesRequest } from "~background/types/CatchNetflixSubtitlesRequest"
 import type { CatchNetflixSubtitlesResponse } from "~background/types/CatchNetflixSubtitlesResponse"
+import replaceXmlBreakTags from "~background/utils/functions/replaceXmlBreakTags"
+import { BREAK_TAG_RENAME } from "~utils/constants"
 
 type XMLText = {
     $: any
     _?: string
-    span?: XMLText
+    span?: XMLText[]
 }
 
-const getXMLTextContent = (text: XMLText) => {
-    return (
-        text.span && text.span[0]
-            ? (text._ ?? "") + " " + getXMLTextContent(text.span[0])
-            : text._ ?? ""
-    ).trim()
+const breakTagRegex = new RegExp(`${BREAK_TAG_RENAME}`, "g")
+
+function getXMLTextContent(text: XMLText): string[] {
+    const theseStrings: string[] = [text._?.trim() ?? ""]
+
+    if (text.span && text.span[0]) {
+        getXMLTextContent(text.span[0]).forEach((spanText: string) => {
+            theseStrings.push(spanText)
+        })
+    }
+
+    const toReturn = []
+    for (const string of theseStrings) {
+        if (string) {
+            toReturn.push(string.replace(breakTagRegex, "<br/>"))
+        }
+    }
+    return toReturn
 }
 
 const handler: PlasmoMessaging.MessageHandler<
@@ -30,31 +44,38 @@ const handler: PlasmoMessaging.MessageHandler<
         message.response?.length > 0
     ) {
         console.log("Caught netflix subtitles: ", message)
-        parseString(message.response, async function (err, result) {
-            if (err) {
-                console.error("Error parsing XML: ", err)
-                return res.send({ error: "Error parsing XML" })
-            }
-            const allText: XMLText[] = result.tt.body?.[0]?.div?.[0]?.p
-            const grouping = {}
-            allText.forEach((text: XMLText) => {
-                const textContent = getXMLTextContent(text)
-                if (grouping[text.$.begin]) {
-                    grouping[text.$.begin].push(textContent)
-                } else {
-                    grouping[text.$.begin] = [textContent]
+        const responseReplaced = replaceXmlBreakTags(message.response)
+        parseString(
+            responseReplaced,
+            {
+                trim: true
+            },
+            async function (err, result) {
+                if (err) {
+                    console.error("Error parsing XML: ", err)
+                    return res.send({ error: "Error parsing XML" })
                 }
-            })
-            const allSentencesSet = new Set<string>()
-            for (const key in grouping) {
-                const sentences = grouping[key]
-                sentences.forEach((sentence: string) => {
-                    allSentencesSet.add(sentence?.trim())
+                const allText: XMLText[] = result.tt.body?.[0]?.div?.[0]?.p
+                const grouping: Record<string, string[]> = {}
+                allText.forEach((text: XMLText) => {
+                    const textContent = getXMLTextContent(text)
+                    if (grouping[text.$.begin]) {
+                        grouping[text.$.begin].push(...textContent)
+                    } else {
+                        grouping[text.$.begin] = [...textContent]
+                    }
                 })
+                const allSentencesSet = new Set<string>()
+                for (const key in grouping) {
+                    const sentences = grouping[key]
+                    sentences.forEach((sentence: string) => {
+                        allSentencesSet.add(sentence?.trim())
+                    })
+                }
+                const allSentencesArray: string[] = Array.from(allSentencesSet)
+                res.send({ netflix_sentences: allSentencesArray })
             }
-            const allSentencesArray: string[] = Array.from(allSentencesSet)
-            res.send({ netflix_sentences: allSentencesArray })
-        })
+        )
     } else {
         return res.send({}) // return empty object if the URL does not have subtitles
     }
