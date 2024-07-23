ba312a03be25c16e4b0668d65b8b59e209ac66c8
demonlexe <alexiskaufman@gmail.com>
2024-07-22 19:40:47 -0500

    init batch sentences when url changes

--- utils/handlers/handleUrlChange.ts
+++ utils/handlers/handleUrlChange.ts
@@ -1,10 +1,12 @@
 import extractIdFromUrl from "~utils/functions/extractMovieFromNetflixUrl"
+import initBatchTranslatedSentences from "~utils/functions/initBatchTranslatedSentences"
 import resetNetflixContext from "~utils/functions/resetNetflixContext"
 
 export default function handleUrlChange() {
     const newShowId = extractIdFromUrl(window.location.href)
     if (newShowId !== window.currentShowId) {
         console.log("URL CHANGED! newShowId:", newShowId)
+        initBatchTranslatedSentences()
         resetNetflixContext()
     }
     window.currentShowId = newShowId
