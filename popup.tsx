ad612d02bb2015aff6aaedf044188e7367886c93
demonlexe <alexiskaufman@gmail.com>
2024-07-25 20:37:26 -0500

    create settings button, header

--- popup.tsx
+++ popup.tsx
@@ -6,20 +6,22 @@ function IndexPopup() {
     const manifestData = chrome.runtime.getManifest()
     return (
         <div className={styles.container}>
-            <button
-                className={styles.redirectButton}
-                onClick={() => {
-                    chrome.tabs.create({
-                        url: `chrome-extension://${chrome.runtime.id}/tabs/onboarding.html`
-                    })
-                }}>
-                Click me to onboard
-            </button>
+            <div className={`${styles.flexRow} ${styles.spaceBetween}`}>
+                <button
+                    className={styles.settingsButton}
+                    onClick={() => {
+                        chrome.tabs.create({
+                            url: `chrome-extension://${chrome.runtime.id}/tabs/onboarding.html`
+                        })
+                    }}>
+                    <span>⚙</span>
+                </button>
+            </div>
+            <DisplayTranslationStatus />
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div>{`NetflixToAnki ${getCurrentYear()}`}</div>
                 <div>v{manifestData.version}</div>
             </div>
-            <DisplayTranslationStatus />
         </div>
     )
 }
