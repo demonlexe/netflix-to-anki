2a6c4db4f5477fe23d90f53acd731f689e5acd5d
demonlexe <alexiskaufman@gmail.com>
2024-07-25 21:05:37 -0500

    open onboarding when installed

--- background/index.ts
+++ background/index.ts
@@ -4,3 +4,12 @@ import { startHub } from "@plasmohq/messaging/pub-sub"
 
 console.log(`Netflix To Anki - Starting Hub`)
 startHub()
+
+chrome.runtime.onInstalled.addListener((reason) => {
+    const reasonStr = reason["reason"] || ""
+    if (reasonStr.includes("install")) {
+        chrome.tabs.create({
+            url: `chrome-extension://${chrome.runtime.id}/tabs/onboarding.html`
+        })
+    }
+})
