e76e2c15e56c7a49696277c9003fbcac06629e1c
demonlexe <alexiskaufman@gmail.com>
2024-07-28 10:06:34 -0500

    wrap with a note

--- components/TranslateWhenSettings.tsx
+++ components/TranslateWhenSettings.tsx
@@ -29,41 +29,44 @@ export default function TranslateWhenSettings<T>() {
     }, [customTranslateKey])
 
     return (
-        <div className={styles.flexRow}>
+        <div className={styles.flexCol}>
             <div className={styles.flexRow}>
-                <label htmlFor="translateWhen">
-                    {READABLE_DROPDOWN_SETTINGS.TRANSLATE_WHEN.title}
-                </label>
-                <select
-                    id="translateWhen"
-                    value={currentSelected}
-                    onChange={(e) =>
-                        setCurrentSelected(
-                            e.target.value as UserSettings["TRANSLATE_WHEN"]
-                        )
-                    }>
-                    {Object.entries(
-                        READABLE_DROPDOWN_SETTINGS.TRANSLATE_WHEN.options
-                    ).map(
-                        ([key, value]: [
-                            UserSettings["TRANSLATE_WHEN"],
-                            string
-                        ]) => (
-                            <option key={key} value={key}>
-                                {value}
-                            </option>
-                        )
-                    )}
-                </select>
-            </div>
-            {currentSelected === "custom_key" && (
-                <div>
-                    <PressAnyKey
-                        keyRecorded={customTranslateKey}
-                        setKeyRecorded={setCustomTranslateKey}
-                    />
+                <div className={styles.flexRow}>
+                    <label htmlFor="translateWhen">
+                        {READABLE_DROPDOWN_SETTINGS.TRANSLATE_WHEN.title}
+                    </label>
+                    <select
+                        id="translateWhen"
+                        value={currentSelected}
+                        onChange={(e) =>
+                            setCurrentSelected(
+                                e.target.value as UserSettings["TRANSLATE_WHEN"]
+                            )
+                        }>
+                        {Object.entries(
+                            READABLE_DROPDOWN_SETTINGS.TRANSLATE_WHEN.options
+                        ).map(
+                            ([key, value]: [
+                                UserSettings["TRANSLATE_WHEN"],
+                                string
+                            ]) => (
+                                <option key={key} value={key}>
+                                    {value}
+                                </option>
+                            )
+                        )}
+                    </select>
                 </div>
-            )}
+                {currentSelected === "custom_key" && (
+                    <div>
+                        <PressAnyKey
+                            keyRecorded={customTranslateKey}
+                            setKeyRecorded={setCustomTranslateKey}
+                        />
+                    </div>
+                )}
+            </div>
+            <div>(Note: this setting automatically saves)</div>
         </div>
     )
 }
