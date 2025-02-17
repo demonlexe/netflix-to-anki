e8a9c697a491ee161b974a5471a20a15ac597b48
demonlexe <alexiskaufman@gmail.com>
2025-02-17 14:01:01 -0800

    improve netflix sub styles

--- utils/functions/changeText.ts
+++ utils/functions/changeText.ts
@@ -19,10 +19,16 @@ const doChangeText = (
     $(span).html(newText)
     $(span).css("color", color)
 
+    // style with font-size:27px;line-height:normal;font-weight:normal;color:#ffffff;text-shadow:#000000 0px 0px 7px;font-family:Netflix Sans,Helvetica Nueue,Helvetica,Arial,sans-serif;font-weight:bolder
     if (window.usingSite === "netflix") {
         $(span).css({
-            "font-size": "19px",
-            "white-space": "normal"
+            "font-size": "4rem",
+            "white-space": "normal",
+            "line-height": "normal",
+            "text-shadow": "#000000 0px 0px 7px",
+            "font-family":
+                "Netflix Sans,Helvetica Nueue,Helvetica,Arial,sans-serif",
+            "font-weight": "bolder"
         })
     }
 }
