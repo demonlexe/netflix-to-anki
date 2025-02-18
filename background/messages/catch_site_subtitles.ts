a79b5974c4d07c7719a5fdbff8b2ef09c5dd9022
demonlexe <alexiskaufman@gmail.com>
2025-02-17 20:04:41 -0800

    replace xml2js with xamel

--- background/messages/catch_site_subtitles.ts
+++ background/messages/catch_site_subtitles.ts
@@ -1,4 +1,4 @@
-import { parseString } from "xml2js"
+import { parse } from "xamel"
 
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
@@ -6,26 +6,23 @@ import { type CatchSiteSubtitlesRequest } from "~background/types/CatchSiteSubti
 import type { CatchSiteSubtitlesResponse } from "~background/types/CatchSiteSubtitlesResponse"
 import logDev from "~utils/functions/logDev"
 
-type XMLText = {
-    $: any
-    _?: string
-    span?: XMLText[]
-    br?: ""
+type Tag = {
+    children: (Tag | string)[]
+    name: string
 }
 
-function getXMLTextContent(text: XMLText): string {
-    const theseStrings: string[] = [text._?.trim() ?? ""]
+function getXMLTextContentFromTag(tag: Tag): string {
+    const theseStrings: string[] = []
 
-    if (text.span && text.span[0]) {
-        // // theseStrings.push(getXMLTextContent(text.span[0]))
-        // theseStrings.unshift(getXMLTextContent(text.span[0]))
-        // // loop all children of text.span
-
-        for (let i = 0; i < text.span.length; i++) {
-            theseStrings.push(getXMLTextContent(text.span[i]))
-            if (text.br && i == 0) {
-                theseStrings.push("<br/>")
-            }
+    // loop children of tag
+    for (let i = 0; i < tag.children.length; i++) {
+        const child = tag.children[i]
+        if (typeof child === "string") {
+            theseStrings.push(child)
+        } else if (child.name === "br") {
+            theseStrings.push("<br/>")
+        } else {
+            theseStrings.push(getXMLTextContentFromTag(child))
         }
     }
 
@@ -45,43 +42,29 @@ const handler: PlasmoMessaging.MessageHandler<
     const { message } = req.body
     if (message.response?.length > 0) {
         logDev("Caught site subtitles: ", message)
-        parseString(
-            message.response,
-            {
-                trim: true
-            },
-            async function (err, result) {
-                logDev("PARSE RESULT: ", result)
+        parse(message.response, (err, xml) => {
+            if (!err) {
                 if (err) {
                     console.error("Error parsing XML: ", err)
                     return res.send({ error: "Error parsing XML" })
                 }
-                // might contain multiple divs
-                const parentDiv = result.tt.body?.[0]?.div
-                const allText: XMLText[] =
-                    parentDiv.length > 1
-                        ? parentDiv.map((div: any) => div?.p?.[0])
-                        : parentDiv?.[0]?.p
-                const grouping: Record<string, string[]> = {}
-                allText.forEach((text: XMLText) => {
-                    const textContent = getXMLTextContent(text)
-                    if (grouping[text.$.begin]) {
-                        grouping[text.$.begin].push(textContent)
-                    } else {
-                        grouping[text.$.begin] = [textContent]
-                    }
-                })
+                const arrayOfRows: { children: Tag[] } = xml
+                    .$("tt/body")
+                    .eq(0)
+                    .$("div")
+                    .eq(0)
+                const allText = arrayOfRows.children
+                logDev("Children: ", allText)
                 const allSentencesSet = new Set<string>()
-                for (const key in grouping) {
-                    const sentences = grouping[key]
-                    sentences.forEach((sentence: string) => {
-                        allSentencesSet.add(sentence?.trim())
-                    })
-                }
+                allText.forEach((text: Tag) => {
+                    const textContent = getXMLTextContentFromTag(text)
+                    allSentencesSet.add(textContent?.trim())
+                })
                 const allSentencesArray: string[] = Array.from(allSentencesSet)
+                logDev("All sentences: ", allSentencesArray)
                 res.send({ site_sentences: allSentencesArray })
             }
-        )
+        })
     } else {
         return res.send({}) // return empty object if the URL does not have subtitles
     }
